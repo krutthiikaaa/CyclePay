2f5cb35912b91b26d27aced39215d74b77a2bc6c
krutthiikaaa <kruthikakonni536@gmail.com>
2026-07-23 20:42:36 +0400

    mongo db connected

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 const app = require("./app");
 
 dotenv.config();
 
+// Connect Database
+connectDB();
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`🚀 Server running on port ${PORT}`);
 });
